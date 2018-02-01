3d4030c76ac16c5edc1dbfd718d45c0527b47fa8
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-02-01 13:48:48 -0200

    Correções nos arquivos html

--- posts/models.py
+++ posts/models.py
@@ -1,22 +1,21 @@
-from django.db import models
-from django.urls import reverse
 from django.conf import settings
+from django.urls import reverse
+from django.db import models
 
 import misaka
 
-from groups.models import Group
-# POSTS MODELS.PY
+from groups.models import  Group
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, related_name = 'posts')
-    created_at =  models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(User, related_name="posts", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
-    message_html = models.TextField(editable = False)
-    group = models.ForeignKey(Group, related_name = 'posts', null = True, blank = True)
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name="posts",null=True, blank=True , on_delete = models.CASCADE)
 
     def __str__(self):
         return self.message
@@ -26,8 +25,14 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('posts:single', kwargs = {'username' : self.user.username, 'pk' : self.pk })
+        return reverse(
+            "posts:single",
+            kwargs={
+                "username": self.user.username,
+                "pk": self.pk
+            }
+        )
 
     class Meta:
-        ordering = ['-created_at']
-        unique_together = ['user', 'message']
+        ordering = ["-created_at"]
+        unique_together = ["user", "message"]
--- posts/views.py
+++ posts/views.py
@@ -1,29 +1,33 @@
-# POSTS VIEWS.PY
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
 
+from django.urls import reverse_lazy
 from django.http import Http404
 from django.views import generic
 
 from braces.views import SelectRelatedMixin
 
+from . import forms
 from . import models
-form . import forms
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
 class PostList(SelectRelatedMixin, generic.ListView):
     model = models.Post
-    select_related = ('user', 'group')
+    select_related = ("user", "group")
+
 
 class UserPosts(generic.ListView):
     model = models.Post
-    template_name = 'posts/user_post_list.html'
+    template_name = "posts/user_post_list.html"
 
     def get_queryset(self):
         try:
-            self.post.user = User.objects.prefetch_related('posts').get(username__iexact = self.kwargs.get('username'))
+            self.post_user = User.objects.prefetch_related("posts").get(
+                username__iexact=self.kwargs.get("username")
+            )
         except User.DoesNotExist:
             raise Http404
         else:
@@ -31,24 +35,33 @@ class UserPosts(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post_user'] = self.post_user
+        context["post_user"] = self.post_user
         return context
+
 
 class PostDetail(SelectRelatedMixin, generic.DetailView):
     model = models.Post
-    select_related = ('user', 'group')
+    select_related = ("user", "group")
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        return queryset.filter(user__username__iexact = self.kwargs.get('username'))
+        return queryset.filter(
+            user__username__iexact=self.kwargs.get("username")
+        )
 
 
 class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
-    fields = ('message', 'group')
+    # form_class = forms.PostForm
+    fields = ('message','group')
     model = models.Post
 
-    def form_valid(self,form):
-        self.object = form.save(commit = False)
+    # def get_form_kwargs(self):
+    #     kwargs = super().get_form_kwargs()
+    #     kwargs.update({"user": self.request.user})
+    #     return kwargs
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.save()
         return super().form_valid(form)
@@ -56,13 +69,13 @@ class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
 
 class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
     model = models.Post
-    select_related = ('user', 'group')
-    success_url = reverse_lazy('posts:all')
+    select_related = ("user", "group")
+    success_url = reverse_lazy("posts:all")
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        return queryset.filter(user_id = self.request.user.id)
+        return queryset.filter(user_id=self.request.user.id)
 
     def delete(self, *args, **kwargs):
-        messages.sucess(self.request,'Post Deleted')
+        messages.success(self.request, "Post Deleted")
         return super().delete(*args, **kwargs)
